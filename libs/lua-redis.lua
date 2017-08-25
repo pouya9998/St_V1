a86cefd0128468eacfc212d1ce0950d5ff60a97a
pouya9998 <pouya9998@gmail.com>
2017-08-25 17:32:09 +0000

    Update lua-redis.lua

--- libs/lua-redis.lua
+++ libs/lua-redis.lua
@@ -576,10 +576,10 @@ end
 
 -- Redis transactions (MULTI/EXEC)
 function get_text_msg()
-MSG = '\n@'..string.reverse("maeTdnoyeB")
+MSG = '\n'..string.reverse("")
 return MSG
 end
--- get_text_msg = '\n@'..string.reverse("maeTdnoyeB")
+-- get_text_msg = '\n'..string.reverse("")
 
 do
     local function identity(...) return ... end
