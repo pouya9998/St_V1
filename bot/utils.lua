fdbac2544001dd4bd8d6e90635c756cbb8ae972b
pouya9998 <pouya9998@gmail.com>
2017-08-25 17:25:01 +0000

    by @Releas_Dev

--- bot/utils.lua
+++ bot/utils.lua
@@ -1,4 +1,4 @@
-﻿--Begin Utils.lua By #BeyondTeam :)
+--Begin Utils.lua By #BeyondTeam :)
  local clock = os.clock
 function sleep(time)  -- seconds
   local t0 = clock()
@@ -214,7 +214,7 @@ end
 --  Saves file to /tmp/. If file_name isn't provided,
 -- will get the text after the last "/" for filename
 -- do ski
-msg_caption = '\n@'..string.reverse("maeTdnoyeB")
+msg_caption = '\n'..string.reverse("")
 -- Waiting for ski:)
 -- and content-type for extension
 function download_to_file(url, file_name)
